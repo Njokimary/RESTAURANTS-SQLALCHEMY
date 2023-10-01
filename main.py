e42036ec309205a64731e236885cef780ed75e72
Njokimary <marynjoki738@gmail.com>
2023-10-01 13:55:26 +0300

    feat:class method init

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sqlalchemy
-from sqlalchemy import Column,String,Integer,create_engine
+from sqlalchemy import Column,String,Integer,create_engine,relationship,ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -13,15 +13,31 @@ class Restaurant(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String)
     price = Column(Integer)
+    reviews = relationship('Review', back_populates='restaurant')
+    customers = relationship('Customer', secondary='reviews', back_populates='restaurants')
+    def all_reviews(self):
+        return [f"Review for {self.name} by {review.customer.full_name()}: {review.rating} stars." for review in self.reviews]
 
 class Customer(Base):
     __tablename__ = 'customers'
     id = Column(Integer, primary_key=True)
     first_name = Column(String)
     last_name = Column(String)
+    reviews = relationship('Review', back_populates='customer')
+    restaurants = relationship('Restaurant', secondary='reviews', back_populates='customers')
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+
 
 class Review(Base):
     __tablename__ = 'reviews'
     id = Column(Integer, primary_key=True)
     rating = Column(Integer)
     comment = Column(String)
+    restaurant_id = Column(Integer, ForeignKey('restaurants.id'))
+    customer_id = Column(Integer, ForeignKey('customers.id'))
+    restaurant = relationship('Restaurant', back_populates='reviews')
+    customer = relationship('Customer', back_populates='reviews')
+    def full_review(self):
+        return f"Review for {self.restaurant.name} by {self.customer.full_name()}: {self.rating} stars."
